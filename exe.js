f7c795575ca993d39bae1aaa7b020996aa6783e7
Ahmedbennaya <aopenking95@gmail.com>
2024-04-26 21:58:03 +0200

    Checkpoint Asynchronous Programming in JavaScript

---
+++ exe.js
@@ -0,0 +1,30 @@
+//Task 01
+// async function iterateWithAsyncAwait(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//       console.log(arr[i]);
+//       await new Promise((resolve) => setTimeout(resolve, 1000));
+//     }
+//   }
+  
+
+//   const myArray = [1, 2, 3, 4, 5];
+//   iterateWithAsyncAwait(myArray);
+//Task 02
+// async function name() {
+//     let data = await fetch("https://api.github.com/users/Ahmedbennaya")
+//     console.log(await data.json());
+// }
+// console.log(name())
+//Task 03
+
+// task 3
+// async function awaitCall() {
+//       try {
+//         let data = await fetch("https://api.github.com/users/Ahmedbennaya");
+//         console.log(await data.json());
+//       } catch (error) {
+//         console.log(`Error is: ${error}`);
+//       }
+//     }
+//     awaitCall();
+
